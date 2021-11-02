5530b2ccd3d2fd13dc710811d8e5d729df48878d
Admin <Admin@MYNEWLAPTOP>
2021-11-03 01:20:52 +0300

    Добавьте файлы проекта.

---
+++ FloorTilingOptimization/Beam.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace FloorTilingOptimization
+{
+    public enum BeamLocationReference
+    {
+        BottomLeft,
+        TopLeft,
+        BottomRight,
+        TopRight
+    }
+
+    public class Beam
+    {
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public Point Location { get; set; }
+        public BeamLocationReference Reference { get; set; }
+        public int RequiredOverlap { get; set; }
+    }
+}
---
+++ FloorTilingOptimization/Sheet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FloorTilingOptimization
+{
+    public class Sheet
+    {
+        public int Width { get; set; }
+        public int Length { get; set; }
+        public int Thickness { get; set; }
+    }
+}
